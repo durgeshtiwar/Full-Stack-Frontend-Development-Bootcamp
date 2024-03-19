a253358d13706b648643f0d834d920a9b3dced49
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-03-19 16:15:17 +0530

    Functions Added

--- JAVASCRIPT/Exercise3.js
+++ JAVASCRIPT/Exercise3.js
@@ -1,3 +1,20 @@
 let computerChoice = '';
 let randomNumber;
 let resultMessage = '';
+function genrateRandomNumber()
+{
+    randomNumber = (Math.random()*3);
+    if(randomNumber > 0 &&  randomNumber <= 1) 
+    {
+        computerChoice = 'Bat';
+        // console.log('Computer Choice is Bat');
+    }else if (randomNumber > 1 &&  randomNumber <= 2 ) {
+        computerChoice = 'Ball';
+        // console.log('Computer Choice is Ball');
+    }
+    else{
+        computerChoice = 'Stump';
+        // console.log('Computer Choice is stump');
+    }
+    console.log(`Computer Choice is ${computerChoice}`);
+}

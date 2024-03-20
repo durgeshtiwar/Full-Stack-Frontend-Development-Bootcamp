555ebc30dfc43ead87977850f92b48dd828fcb04
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-03-20 20:59:02 +0530

    Objects

--- JAVASCRIPT/Exercise3.js
+++ JAVASCRIPT/Exercise3.js
@@ -1,6 +1,11 @@
 let computerChoice = '';
 let randomNumber;
 let resultMessage = '';
+let  score = {
+    Win : 0,
+    Lose : 0,
+    Tie : 0,
+};
 function genrateRandomNumber()
 {
     randomNumber = (Math.random()*3);
@@ -22,28 +27,44 @@ function checkWinner(userInput, computerChoice)
 {
     if (userInput === 'Bat') {
         if (computerChoice === 'Bat') {
+            score.Tie++;
             return `It's is Tie`;
         } else if (computerChoice === 'Ball') {
+            score.Win++;
             return `User Win`;
         }else if (computerChoice === 'Stump') {
+            score.Lose++ ;
             return `Computer Win`;
         }
     } else if (userInput ==='Ball') {
         if (computerChoice === 'Bat') {
+            score.Lose++ ;
             return `Computer Win`;
         } else if (computerChoice === 'Ball') {
+            score.Tie++;
             return `It's is Tie`;
         }else if (computerChoice === 'Stump') {
+            score.Win++;
             return `User Win`;
         }
     } else
     {
         if (computerChoice === 'Bat') {
+            score.Win++;
             return `User Win`;
         } else if (computerChoice === 'Ball') {
+            score.Lose++ ;
             return `Computer Win`;
         }else if (computerChoice === 'Stump') {
+            score.Tie++;
             return `It's is Tie`;
         }
     }
+}
+function displayResult(yourChoice,computerChoice,resultMessage)
+{
+    alert(`Your Choice is : ${yourChoice}
+Computer Choice is : ${computerChoice}
+The Result is : ${resultMessage}
+Win:${score.Win}, Lose:${score.Lose}, Tie:${score.Tie}`);
 }

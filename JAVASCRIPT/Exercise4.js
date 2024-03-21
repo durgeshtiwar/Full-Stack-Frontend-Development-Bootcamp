020176794d2e8839f85849b858eb85abade69c2e
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-03-21 11:53:49 +0530

    Objects Functionality

---
+++ JAVASCRIPT/Exercise4.js
@@ -0,0 +1,38 @@
+// let watch = {
+//     price : 500,
+//     compony : 'HMT',
+//     color :  'Black',
+//     size: 'M',
+// }
+// let  phone = {
+//     brand : 'Samsung',
+//     model : 'Galaxy S2',
+//     color : 'Black',
+//     price : 30000,
+//     job : {
+//         message : 'good job',
+//         status : 'complete',
+//     }
+// }
+// console.log(watch);
+// let Apple = watch;
+// let fastTrak = watch;
+// fastTrak.color='White';
+// console.log(watch);
+// console.log(Apple);
+// console.log(fastTrak);
+// console.log(phone['brand']); 
+
+// let message = phone.job.message;
+// console.log(typeof message) ;
+// let{status} = phone.job;
+// console.log(typeof status);
+
+// function isIdenticalProduct() {
+//     if (watch['color'] === phone['color']) {
+//         console.log("They are identical product");
+//     } else {
+//         console.log('Not Identical Product');
+//     }
+// }
+// isIdenticalProduct();

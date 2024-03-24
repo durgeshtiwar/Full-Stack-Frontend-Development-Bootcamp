3bbe29abfff2dff8c0a589b389023adbd1c455c2
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-03-24 19:36:29 +0530

    Todo Functionality

--- JAVASCRIPT/Exercise7.js
+++ JAVASCRIPT/Exercise7.js
@@ -1,8 +1,29 @@
-let arrlist1 = [];
+let arrlist1 = [{Item : 'vbcc',dueDate : '05/06/2033'}];
+displayTodo();
 function addTodo()
 {
     let todoInput = document.querySelector('.todo-Input');
+    let dateInput = document.querySelector('.date-Input');
     let todoValue = todoInput.value;
-    arrlist1.push(todoValue);
+    let dateValue = dateInput.value;
+    arrlist1.push({Item : todoValue , dueDate : dateValue});
     todoInput.value="";
+    dateInput.value="";
+    displayTodo();
+}
+function displayTodo()
+{
+    let containerElement = document.querySelector('.container');
+    let newHTML = '';
+    for (let index = 0; index < arrlist1.length; index++) {
+        let { Item , dueDate }=arrlist1[index] ;
+        newHTML += `
+        <span>${Item}</span>
+        <span>${dueDate}</span>
+        <button class="btn-delete" onclick="
+        arrlist1.splice(${index},1);
+        displayTodo();">Delete</button>
+        `
+    }
+    containerElement.innerHTML = newHTML;
 }

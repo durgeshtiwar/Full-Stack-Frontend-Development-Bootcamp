63fc7ebe2462811c26b5ffd93949d3fa9e7d37cd
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-03-28 22:19:35 +0530

    Event listner

--- JAVASCRIPT/Exercise8.js
+++ JAVASCRIPT/Exercise8.js
@@ -10,13 +10,6 @@
 // console.log(runTwice(runMethod));
 
 
-// let button = document.querySelector(".button");
-// function doubleSize (){ button.addEventListener("click", () =>{
-//     console.log("Button clicked");
-//     button.style.height = '50px';
-//     button.style.width = '100px';
-// })};
-
 // let arr1 = [1,5,7,8,9,6,4,3,5];
 // let sums = 0;
 // arr1.forEach(element => {
@@ -29,3 +22,13 @@
 //     num = num*num;
 //     console.log(num);
 // })
+
+
+
+// let button = document.querySelector(".button");
+// button.addEventListener( "click", event =>setTimeout(doubleTheSize, 2000));
+// function doubleTheSize(){
+//     console.log("Button clicked");
+//     button.style.height = '50px';
+//     button.style.width = '100px';
+// }

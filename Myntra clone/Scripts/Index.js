73572932dfa907f9bedd84409816a53b988ea9fb
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-03-31 22:44:12 +0530

    Add to bag in myntra

--- Myntra clone/Scripts/Index.js
+++ Myntra clone/Scripts/Index.js
@@ -1,18 +1,29 @@
-let itemsContainerElement =  document.getElementById(".item-containers");
-console.log("This is inside index");
-itemsContainerElement.innerHTML = `
-<div class="item-container">
-    <img class="item-img" src="${items.image}" alt="item image">
-    <div class="rating">
-    ${items.rating.stars} ⭐ | ${items.rating.count}
-    </div>
-    <div class="compony-name">${items.company}</div>
-    <div class="item-name">${items.item_name}</div>
-    <div class="price">
-        <span class="current-price">Rs ${items.current_price}</span>
-        <span class="original-price">Rs ${items.original_price}</span>
-        <span class="discount">(${items.discount_percentage}% OFF)</span>
-    </div>
-    <button class="btn-add-bags">Add to Bag</button>
-
-</div>`;
+let bagItems = [];
+displayItemsOnHomepage();
+function addToBag(itemid)
+{
+    bagItems.push(itemid);
+}
+function displayItemsOnHomepage()
+{
+    let itemsContainerElements = document.querySelector(".item-containers");
+    let innerHTML = '';
+    items.forEach(items => {
+        innerHTML+=`<div class="item-container">
+        <img class="item-img" src="${items.image}" alt="item image">
+        <div class="rating">
+        ${items.rating.stars} ⭐ | ${items.rating.count}
+        </div>
+        <div class="compony-name">${items.company}</div>
+        <div class="item-name">${items.item_name}</div>
+        <div class="price">
+            <span class="current-price">Rs ${items.current_price}</span>
+            <span class="original-price">Rs ${items.original_price}</span>
+            <span class="discount">(${items.discount_percentage}% OFF)</span>
+        </div>
+        <button class="btn-add-bags" onclick = "addToBag(${items.id});">Add to Bag</button>
+        
+        </div>`;
+    });
+    itemsContainerElements.innerHTML = innerHTML;
+}

9d046799d86cb2825834e8dfddaf4ba6236cbe78
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-04-04 18:49:32 +0530

    Assignment upto 15

--- JAVASCRIPT/Exercise9.js
+++ JAVASCRIPT/Exercise9.js
@@ -49,7 +49,6 @@
 // 10. Boolean value is either true or false.
 //     - Write three JavaScript statement which provide truthy value.
 //     - Write three JavaScript statement which provide falsy value.
-
 // const arr1 = ['false', '0', 42,-42, " ", new Date (),Infinity,-Infinity];
 // if (arr1[7]) {
 //     console.log('This is a Truthy Value');
@@ -63,3 +62,75 @@
 //     console.log('This is a Falsy Value');
 // }
 
+// 11. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//     - 4 > 3
+//     - 4 >= 3
+//     - 4 < 3
+//     - 4 <= 3
+//     - 4 == 4
+//     - 4 === 4
+//     - 4 != 4
+//     - 4 !== 4
+//     - 4 != '4'
+//     - 4 == '4'
+//     - 4 === '4'
+//     - Find the length of python and jargon and make a falsy comparison statement.
+// console.info(4 > 3);
+// console.info(4 >= 3);
+// console.info(4 < 3);
+// console.info(4 <= 3);
+// console.info(4 == 4);
+// console.info(4 === 4);
+// console.info(4 != 4);
+// console.info(4 !== 4);
+// console.info(4 != '4');
+// console.info(4 == '4');
+// console.info(4 === '4');
+// console.log("\n");
+// console.log(4 > 3);
+// console.log(4 >= 3);
+// console.log(4 < 3);
+// console.log(4 <= 3);
+// console.log(4 == 4);
+// console.log(4 === 4);
+// console.log(4 != 4);
+// console.log(4 !== 4);
+// console.log(4 != '4');
+// console.log(4 == '4');
+// console.log(4 === '4');
+// console.log("\n");
+// console.log('python'.length !== 'jargon'.length);
+
+// 12. Use the Date object to do the following activities
+//     - What is the year today?
+//     - What is the month today as a number?
+//     - What is the date today?
+//     - What is the day today as a number?
+//     - What is the hours now?
+//     - What is the minutes now?
+//     - Find out the numbers of seconds elapsed from January 1, 1970 to now.
+// let date = new Date();
+// console.log(date.getFullYear());
+// console.log(date.getMonth() + 1);
+// console.log(date.toLocaleDateString()); 
+// console.log(date.getDay());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// let givenDate = new Date('1990-01-01');
+// let elapsed = date - givenDate;
+// console.log(elapsed / 1000);
+
+//13. Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+// let base = prompt('Pleas Enetr Base');
+// let height = prompt('Pleas Enetr height');
+// console.log(`The area of traingle is ${0.5 * base * height} square`);
+
+//14. Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
+// let x1 = 2;
+// let y1 = 2;
+// let x2 = 6;
+// let y2 = 10;
+// console.log("Slope is = "+(y2 - y1) / (x2 - x1));
+
+//15. Calculate the slope, x-intercept and y-intercept of y = 2x -2
+//console.log("Slop is 2");

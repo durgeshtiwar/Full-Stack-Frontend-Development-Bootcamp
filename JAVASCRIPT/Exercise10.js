4cd8607a6a7d912331f5842f3b07045f6d2e4066
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-04-12 19:05:16 +0530

    BMI Calculater

--- JAVASCRIPT/Exercise10.js
+++ JAVASCRIPT/Exercise10.js
@@ -1,7 +1,15 @@
-const body = document.querySelector('body');
-const btn = document.querySelectorAll('.button');
-for (let index = 0; index < btn.length; index++) {
-    btn[index].addEventListener('click', function () {
-    body.style.backgroundColor = `${btn[index].id}`;
-    });
-}
+const form = document.querySelector('form');
+form.addEventListener("submit", (event) => {
+event.preventDefault();
+const height = parseInt(document.querySelector('#height').value);
+const weight = parseInt(document.querySelector('#weight').value);
+const result = document.querySelector('#results');
+if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+}}); 

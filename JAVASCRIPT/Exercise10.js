df1e46e141b26b8cff96ad4777750cab6e075ea2
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-04-12 19:15:15 +0530

    SomeChanges

--- JAVASCRIPT/Exercise10.js
+++ JAVASCRIPT/Exercise10.js
@@ -5,11 +5,11 @@ const height = parseInt(document.querySelector('#height').value);
 const weight = parseInt(document.querySelector('#weight').value);
 const result = document.querySelector('#results');
 if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please give a valid height ${height}`;
+    result.innerHTML = `Please give a valid height ${height}`;
   } else if (weight === '' || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please give a valid weight ${weight}`;
+    result.innerHTML = `Please give a valid weight ${weight}`;
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+    result.innerHTML = `<span>${bmi}</span>`;
 }}); 

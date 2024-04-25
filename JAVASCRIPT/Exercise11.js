14269ef78bd07cb1ebcfd68b83aee95b01372799
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-04-25 17:08:22 +0530

    Events Keycodes

--- JAVASCRIPT/Exercise11.js
+++ JAVASCRIPT/Exercise11.js
@@ -1,25 +1,51 @@
-let randomColor = function genrateRandomColor()
-{
-    const hex = "0123456789ABCDEF";
-    let color = "#";
-    for(let i=0;i<6;i++){
-        color += hex[Math.floor(Math.random()*16)];
-    }
-    return color;
-}
-let changeColorId;
-document.querySelector("#start").addEventListener('click', (e) => 
-{
-    let changeColor = function () {
-        document.body.style.backgroundColor = randomColor();
-    }
-    if (!changeColorId) {
-        changeColorId = setInterval(changeColor, 1000);
-    }
-}); 
+// let randomColor = function genrateRandomColor()
+// {
+//     const hex = "0123456789ABCDEF";
+//     let color = "#";
+//     for(let i=0;i<6;i++){
+//         color += hex[Math.floor(Math.random()*16)];
+//     }
+//     return color;
+// }
+// let changeColorId;
+// document.querySelector("#start").addEventListener('click', (e) => 
+// {
+//     let changeColor = function () {
+//         document.body.style.backgroundColor = randomColor();
+//     }
+//     if (!changeColorId) {
+//         changeColorId = setInterval(changeColor, 1000);
+//     }
+// }); 
 
-document.querySelector("#stop").addEventListener('click', (e) => 
+// document.querySelector("#stop").addEventListener('click', (e) => 
+// {
+//    clearInterval(changeColorId);
+//    changeColorId = null;
+// }); 
+
+
+// Key
+
+
+let insert = document.querySelector('#insert');
+window.addEventListener("keydown", (e)=>
 {
-   clearInterval(changeColorId);
-   changeColorId = null;
-}); 
+    insert.innerHTML = `
+    <div class='color'>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th> 
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td> 
+      <td>${e.code}</td>
+    </tr>
+    
+  </table>
+    </div>
+  `;
+});

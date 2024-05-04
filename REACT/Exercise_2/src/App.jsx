e9f0af01059733969f43dd40206dffb27e3134d4
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-05-04 19:49:07 +0530

    Bharat Clock UI

---
+++ REACT/Exercise_2/src/App.jsx
@@ -0,0 +1,16 @@
+import AppName from './components/AppName'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Appslogan from './components/Appslogan'
+import AppTime from './components/AppTime'
+function App() {
+  
+  return (
+    <center>
+    <AppName/>
+    <Appslogan/>
+    <AppTime/>
+    </center>
+  )
+}
+
+export default App
---
+++ REACT/Exercise_2/src/components/AppName.jsx
@@ -0,0 +1,9 @@
+function AppName()
+{
+  return(
+  <div className="clock_container">
+  <h1 className="fw-bolder">Bharat Clock</h1>
+  </div>
+  )
+}
+export default AppName;

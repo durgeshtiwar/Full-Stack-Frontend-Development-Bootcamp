fa6b114e887a8a9c6dd6025a5ba6264390565507
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-05-09 22:21:40 +0530

    Events

--- REACT/Exercise_3/src/compoenets/Fooditems.jsx
+++ REACT/Exercise_3/src/compoenets/Fooditems.jsx
@@ -1,8 +1,13 @@
+import style from "./Fooditems.module.css";
 const foodItems = ({items})=>{
   return(
     <>
     <ul class="list-group">
-    {items.map(item =>(<li class="list-group-item">{item}</li>))}
+    {items.map(item =>(<li class="list-group-item">{item}
+    <button className={`${style.button} btn btn-info`}
+    onClick={()=>console.log("Button Clicked")}
+    >Buy Now</button>
+    </li>))}
     </ul>
     </>
   );

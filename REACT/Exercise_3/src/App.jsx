d52a1e02455fcae7e8064a951500f0a17aaee2b2
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-05-11 17:09:11 +0530

    Usestate in React

--- REACT/Exercise_3/src/App.jsx
+++ REACT/Exercise_3/src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Container from "./compoenets/Container";
 import FoodInput from "./compoenets/FoodInput";
 import Fooditems from "./compoenets/Fooditems";
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   let healthItems = ["Roti","Dal","Hari Sbjiya","Ghee"];
+  let [textToShow,setTextToShow] = useState();
   const changeOnInput = () => {
-    console.log(event.target.value)
+    console.log(event.target.value);
+    setTextToShow(event.target.value);
   }
   const buttonWasClicked = ()=>{
     console.log("Button Clicked");
@@ -15,6 +18,7 @@ function App() {
     <Container>
     <h1>Fragments, Randring using map, Passing Data Throw Props</h1>
     <FoodInput changeOnInput={changeOnInput}/>
+    <p>{textToShow}</p>
     <Fooditems items={healthItems} buttonWasClicked={buttonWasClicked}
     > </Fooditems>
     </Container>

fb81d35fb056891041e6aa7889c2f9ac1f28b8ca
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-05-12 21:32:11 +0530

    Under Processing todo app 3

--- REACT/Exercise_1/src/components/AddTodo.jsx
+++ REACT/Exercise_1/src/components/AddTodo.jsx
@@ -5,15 +5,23 @@ function AddTodo({addTodo})
   const[todoName,settodoName] = useState();
   const[dueDate,setdueDate] = useState();
 
+  const handleNameChange = (event)=>
+  {
+    console.log(event.target.value);
+  }
+  const handleDateChange = (event)=>
+    {
+      console.log(event.target.value);
+    }
 
   return <>
   <div className="container">
   <div className="row kg-row">
     <div className="col-5">
-      <input type="text" placeholder="Enter your here" onChange={handleNameChange}/>
+      <input type="text" placeholder="Enter your here" onChange={(event)=>handleNameChange(event)}/>
     </div>
     <div className="col-3">
-      <input type="date" />
+      <input type="date" onChange={(event)=>handleDateChange(event)} />
     </div>
     <div className="col-2">
     <button type="button" className="btn btn-success kg-button" onClick={

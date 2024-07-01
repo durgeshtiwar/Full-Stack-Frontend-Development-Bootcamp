f863c64ac268d5840c7e1ca1512727f21eb34732
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-07-01 11:42:54 +0530

    Context API part 1

---
+++ REACT/Exercise_7/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <center>
+    <h1>Chai Aur Code With Context API</h1>
+    </center>
+  )
+}
+
+export default App

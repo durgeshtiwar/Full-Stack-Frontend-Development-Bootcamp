d5f1f399313447631cf2c0958ba3b78a1bb16798
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-07-03 17:45:31 +0530

    Social Media Project

---
+++ REACT/Exercise_8/src/components/PostList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post';
+
+function PostList() {
+  return (
+    <div>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+    </div>
+  )
+}
+
+export default PostList;

2f7175d6b4d172c95864c8a7b9ce1e8c9d4c94e4
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-07-12 17:19:44 +0530

    Start Routing

--- REACT/Exercise_8/src/router/App.jsx
+++ REACT/Exercise_8/src/router/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Sidebar from './components/Sidebar'
-import CreatePost from './components/CreatePost'
-import PostList from './components/PostList'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Sidebar from '../components/Sidebar'
 import { useState } from 'react'
-import PostListProvider from './store/post-list-store'
+import PostListProvider from '../store/post-list-store'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [selectedTab,setSelectedTab] = useState("Home");
@@ -16,7 +15,7 @@ function App() {
     <Sidebar selectedTab={selectedTab} setSelectedTab = {setSelectedTab} />
     <div id='Container'>
     <Header/>
-    {selectedTab === "Home" ? <PostList/> : <CreatePost/>}
+    <Outlet></Outlet>
     <Footer/>
     </div>
     </div>

94a45c5ff541b271fda9747bccec6621e6c6a4c5
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-08-22 22:58:30 +0530

    currency project

--- ADVANCE_REACT/Exercise_2/src/App.jsx
+++ ADVANCE_REACT/Exercise_2/src/App.jsx
@@ -1,7 +1,14 @@
-
+import InputBox from './components/InputBox'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
+import { useState } from 'react'
 function App() {
   
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+
+
   return (
     <>
       <h1 className='text-3xl bg-orange-700'>Chai with custom hooks</h1>

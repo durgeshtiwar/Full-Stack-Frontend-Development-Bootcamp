b46d84d94d756b27b349bcc6791d8ea537db34ee
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-09-17 16:32:54 +0530

    Configure redux toolkit

--- ADVANCE_REACT/Exercise_8/src/App.jsx
+++ ADVANCE_REACT/Exercise_8/src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
-import conf from '../conf/conf';
+
 function App() {
-  console.log(conf.appwriteBucketId);
   return (
     <>
-      <h1>A blog app with Appwrite</h1>
+      <h1 className='bg-slate-600'>A blog app with Appwrite</h1>
     </>
   )
 }
